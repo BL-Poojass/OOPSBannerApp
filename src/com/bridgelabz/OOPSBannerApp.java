9c0a1064ac40e2459ddaa12bde9baee975beb5f5
BL-Poojass <pooja.ss@bridgelabz.com>
2026-01-29 16:07:15 +0530

    [Pooja] Added:UC3-string-join

--- src/com/bridgelabz/OOPSBannerApp.java
+++ src/com/bridgelabz/OOPSBannerApp.java
@@ -1,5 +1,18 @@
 package com.bridgelabz;
 
+/**
+ * UC3 - Banner using String.join()
+ */
 public class OOPSBannerApp {
 
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "  *****  ", "  *****  ", " ******  ", "  ***** "));
+        System.out.println(String.join(" ", " *     * ", " *     * ", " *     * ", " *     *"));
+        System.out.println(String.join(" ", "*       *", "*       *", " *     * ", " *      "));
+        System.out.println(String.join(" ", "*       *", "*       *", " ******  ", "  ***** "));
+        System.out.println(String.join(" ", "*       *", "*       *", " *       ", "      * "));
+        System.out.println(String.join(" ", " *     * ", " *     * ", " *       ", " *    * "));
+        System.out.println(String.join(" ", "  *****  ", "  *****  ", " *       ", "  ***** "));
+    }
 }
