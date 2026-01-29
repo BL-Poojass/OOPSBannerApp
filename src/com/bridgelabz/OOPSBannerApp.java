6f4dd97eb2a416178a0eda3605b37fa3706d7cd3
BL-Poojass <pooja.ss@bridgelabz.com>
2026-01-29 16:17:26 +0530

    [Pooja] Added:UC7-character-class

--- src/com/bridgelabz/OOPSBannerApp.java
+++ src/com/bridgelabz/OOPSBannerApp.java
@@ -1,5 +1,68 @@
 package com.bridgelabz;
 
+/**
+ * UC7 - Character pattern stored in a class
+ */
 public class OOPSBannerApp {
 
+    static class CharacterPattern {
+        private final char character;
+        private final String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    public static CharacterPattern createO() {
+        return new CharacterPattern('O', new String[]{
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        });
+    }
+
+    public static CharacterPattern createP() {
+        return new CharacterPattern('P', new String[]{
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       ",
+                " *       "
+        });
+    }
+
+    public static CharacterPattern createS() {
+        return new CharacterPattern('S', new String[]{
+                "  *****  ",
+                " *     * ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                " *     * ",
+                "  *****  "
+        });
+    }
+
+    public static void main(String[] args) {
+
+        CharacterPattern o = createO();
+        CharacterPattern p = createP();
+        CharacterPattern s = createS();
+
+        for (int i = 0; i < o.getPattern().length; i++) {
+            System.out.println(o.getPattern()[i] + "  " + o.getPattern()[i] + "  " + p.getPattern()[i] + "  " + s.getPattern()[i]);
+        }
+    }
 }
