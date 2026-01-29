f6f1f3e25bbad493007b8243b00d40cf92618971
BL-Poojass <pooja.ss@bridgelabz.com>
2026-01-29 15:14:07 +0530

    [Pooja] Added:UC4-array-loop

--- src/com/bridgelabz/OOPSBannerApp.java
+++ src/com/bridgelabz/OOPSBannerApp.java
@@ -1,5 +1,24 @@
 package com.bridgelabz;
 
+/**
+ * UC4 - Banner using String array and loop
+ */
 public class OOPSBannerApp {
 
+    public static void main(String[] args) {
+
+        String[] lines = {
+                String.join(" ", "  *****  ", "  *****  ", " ******  ", "  ***** "),
+                String.join(" ", " *     * ", " *     * ", " *     * ", " *     *"),
+                String.join(" ", "*       *", "*       *", " *     * ", " *      "),
+                String.join(" ", "*       *", "*       *", " ******  ", "  ***** "),
+                String.join(" ", "*       *", "*       *", " *       ", "      * "),
+                String.join(" ", " *     * ", " *     * ", " *       ", " *    * "),
+                String.join(" ", "  *****  ", "  *****  ", " *       ", "  ***** ")
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
 }
