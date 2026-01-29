7ef73c7035af3c4f022b84f63de871aab9ce8bc5
BL-Poojass <pooja.ss@bridgelabz.com>
2026-01-29 15:01:24 +0530

    [Pooja] Added:UC2-banner-print

--- src/com/bridgelabz/OOPSBannerApp.java
+++ src/com/bridgelabz/OOPSBannerApp.java
@@ -1,5 +1,18 @@
 package com.bridgelabz;
 
+/**
+ * UC2 - Banner using print statements
+ */
 public class OOPSBannerApp {
 
+    public static void main(String[] args) {
+
+        System.out.println("  *****   " + "  *****   " + " ******   " + "  *****  ");
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *     * ");
+        System.out.println("*       * " + "*       * " + " *     *  " + " *       ");
+        System.out.println("*       * " + "*       * " + " ******   " + "  *****  ");
+        System.out.println("*       * " + "*       * " + " *        " + "        *");
+        System.out.println(" *     *  " + " *     *  " + " *        " + " *     * ");
+        System.out.println("  *****   " + "  *****   " + " *        " + "  *****  ");
+    }
 }
