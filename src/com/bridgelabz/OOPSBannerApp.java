e7b59daa110f54800f9e972a349ec4a851f7e3d4
BL-Poojass <pooja.ss@bridgelabz.com>
2026-01-29 15:19:40 +0530

    [Pooja] Added:UC6-method-refactor

--- src/com/bridgelabz/OOPSBannerApp.java
+++ src/com/bridgelabz/OOPSBannerApp.java
@@ -1,5 +1,55 @@
 package com.bridgelabz;
 
+/**
+ * UC6 - Refactor banner logic into methods
+ */
 public class OOPSBannerApp {
 
+    public static String[] getOPattern() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    public static String[] getPPattern() {
+        return new String[]{
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+    }
+
+    public static String[] getSPattern() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
+        }
+    }
 }
+
